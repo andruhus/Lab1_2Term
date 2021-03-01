6c11232c0fc52dce4ba3a4727d39d0aadb6804d5
andruhus <aaademed@gmail.com>
2021-03-02 00:08:16 +0200

    Set up GoogleTest

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
-
+/*
 #include "Triangle.h"
 
 
 int main() {
 
+    return 0;
+}
+*/
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+TEST(FizzBuzzTest, shouldReturn1for1) {
+    ASSERT_EQ(0, 0);
+}
+
+int main(int argc, char *argv[]) {
+    testing::InitGoogleTest(&argc, argv);
+    RUN_ALL_TESTS();
     return 0;
 }
